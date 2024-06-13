2653bc0a4b0152992135b6dd0ad043516c5015fe
DINESH S <119110395+Dinesh-122333@users.noreply.github.com>
2024-06-13 15:59:53 +0530

    added file

--- digonal_sum.py
+++ digonal_sum.py
@@ -15,17 +15,16 @@ mat = [
 
 
 # 8
-def sum_diagonals(mat):
-    
-    if not mat or any(len(row) != len(mat) for row in mat):
-        return "The matrix is not square"
 
-    pri_dig = 0
-    sec_dig = 0
-    
-    
-    for i in range(len(mat)):
-        pri_dig += mat[i][i]
-        sec_dig += mat[i][len(mat) - 1 - i] 
 
-    return pri_dig, sec_dig
+pri_dig = 0
+sec_dig = 0
+
+    
+for i in range(len(mat)):
+    pri_dig += mat[i][i]
+    sec_dig += mat[i][len(mat) - 1 - i] 
+
+# print(pri_dig, sec_dig)
+print(pri_dig + sec_dig)
+
